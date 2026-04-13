409869259b93a265404be1094c48f508ec7ff1a8
Bradly Landucci <salvesoundsofficial@gmail.com>
2026-04-13 13:53:50 -0700

    Added waveshaper

--- fx/waveshaper/waveshaper.cpp
+++ fx/waveshaper/waveshaper.cpp
@@ -1,65 +1,80 @@
-// Created by Bradly Landucci
+/*
+    Created by Bradly Landucci
+*/
 
 #include <usermodfx.h>
-#include "fx_api.h"
+#include <fx_api.h>
 
-// Initializing Params //
-float distAmt;
+#include <cmath>
 
-// Simple waveshaping algorithm //
-// Audio Effects: Theory, Implementation and Application Pg. 184
-float __fast_inline waveshape(float in)
+//--------------------------------------------------------------------------------
+
+constexpr float DriveRangeDb(100.f);
+
+static float drive{ 0.5f };
+static float mix{ 0.5f };
+
+//--------------------------------------------------------------------------------
+
+float waveshape(float in)
 {
-    float threshold1 = 1.0f / 3.0f;
-    float threshold2 = 2.0f / 3.0f;
-
-    if (in > threshold2)
-        return 1.0f;
-    else if (in > threshold1)
-        return (3.0f - (2.0f - 3.0f * in) * (2.0f - 3.0f * in)) / 3.0f;
-    else if (in < -threshold2)
-        return -1.0f;
-    else if (in < -threshold1)
-        return -(3.0f - (2.0f + 3.0f * in) * (2.0f + 3.0f * in)) / 3.0f;
-    else
-        return 2.0f * in;
+    return std::tanh(in);
 }
 
-// Initializing Platform //
+//--------------------------------------------------------------------------------
+
 void MODFX_INIT(uint32_t platform, uint32_t api)
 {
-    float distAmt = 0.5f;
+    (void)platform;
+    (void)api;
 }
 
-// Main DSP Process Block //
-// Sub values to be ignored, only used for prologue
+//--------------------------------------------------------------------------------
+
 void MODFX_PROCESS(const float *xn, float *yn, const float *sub_xn, float *sub_yn, uint32_t frames)
 {
-    // Effect processing loop //
-    // For double frames (AKA samples cause each frame = sample pair)
-    for (int i = 0; i < frames * 2; i++)
-    {
-        float xn_cur = *xn++;
-        const float dry = 1.f - distAmt;
-        const float drive = 100.f;
+    static_assert(DriveRangeDb > 1.f);
 
-        *yn++ = (dry * xn_cur) + (distAmt * waveshape(drive * xn_cur));
+    (void)sub_xn;
+    (void)sub_yn;
+
+    const float *x{ xn };
+    const float *xEnd{ xn + 2 * frames };
+
+    float *y{ yn };
+
+    float dry{ 1.f - mix };
+    float wet{ mix };
+
+    float currentDrive{ drive * (DriveRangeDb - 1.f) + 1.f };
+
+    for (; x != xEnd; x += 2, y += 2)
+    {
+        float inLeft{ *x };
+        float inRight{ *x + 1 };
+
+        *y = (dry * inLeft) + (wet * waveshape(currentDrive * inLeft));
+        *(y + 1) = (inRight) + (wet * waveshape(currentDrive * inRight));
     }
 }
 
-// Param Controls //
+//--------------------------------------------------------------------------------
+
 void MODFX_PARAM(uint8_t index, int32_t value)
 {
     // Convert fixed-point q31 format to float
-    const float valf = q31_to_f32(value);
+    const float valf{ q31_to_f32(value) };
     switch (index)
     {
-    case 0:
-        distAmt = valf;
+    case k_user_modfx_param_time:
+        drive = valf;
         break;
-    case 1:
+    case k_user_modfx_param_depth:
+        mix = valf;
         break;
     default:
         break;
     }
 }
+
+//--------------------------------------------------------------------------------
